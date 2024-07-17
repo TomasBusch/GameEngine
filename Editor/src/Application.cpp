241a607e18329099ea1e6166ae0edd0ddc41db47
Tomibusch1 <tomibusch5@gmail.com>
2024-07-16 21:01:35 -0300

    Add dependencies

---
+++ Editor/src/Application.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <Engine.h>
+#include <config.h>
+
+int main()
+{
+	std::cout << VulkanEngine::cmake::project_name << "Editor" << std::endl;
+	return 0;
+}
--- SandBox/src/Application.cpp
+++ SandBox/src/Application.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <Engine.h>
+#include <config.h>
+#include <fstream>
+#include <filesystem>
 
-int main()
+int main(int argc, char* argv[])
 {
-	std::cout << "Hello CMake from Application." << returnTwo() << std::endl;
+	std::ofstream file;
+	
+	file.open("res/test.txt");
+	if (file.is_open()) {
+		file << "test data ";
+
+		std::cout << VulkanEngine::cmake::project_name << "SandBox" << returnTwo() << std::endl << std::endl;
+	}
+
+	std::cout << std::filesystem::current_path() << std::endl;
+	std::cout << argv[0] << std::endl;
+
 	return 0;
 }
