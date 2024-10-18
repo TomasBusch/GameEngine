bdf4ae3dd7e91a55e1e9559514ef817aa412a59e
Tomibusch1 <tomibusch5@gmail.com>
2024-10-18 00:42:34 -0300

    Add config classes

---
+++ Engine/src/Engine/Core/Config/Config.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "ApplicationConfig.hpp"
+#include "GraphicsConfig.hpp"
+
+namespace Engine {
+
+	struct Config {
+		ApplicationConfig application;
+		GraphicsConfig graphics;
+	};
+}
